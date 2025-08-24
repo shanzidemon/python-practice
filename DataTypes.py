1503a2ae066db2211b4bcae9e1c7f629d7b27062
shanzidemon <shanzidemon@gmail.com>
2025-08-24 10:35:54 +0600

    Add new Python code

---
+++ DataTypes.py
@@ -0,0 +1,16 @@
+#Type check
+x=5
+print(type(x))
+x=5.67
+print(type(x))
+a=5#int
+b=5.7#float
+c=8j#complex
+d=["apple","banana","orange"]#list
+e=("apple","banana","chery")#triple
+
+import random
+print("The value of ransom Number is",random.randrange(1,500))
+x=str(10)
+print(x)
+print(type(x))

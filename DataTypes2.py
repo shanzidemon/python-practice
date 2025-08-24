ae641dd7897ba9e1114dac81d9f732737cbd4fc7
shanzidemon <shanzidemon@gmail.com>
2025-08-24 13:49:16 +0600

    Add new Python code

---
+++ DataTypes2.py
@@ -0,0 +1,20 @@
+a=100#integer NUmber
+b=15.6#float Number
+c=9+7j#complex Number
+d="Shanzid Helal Emoo "#String
+e=["Shanzzid","Helal","Emmon"]#list
+f=("Shanzid""Helal","Emon")#tupale
+g=range(8,10)#range
+h={"name":"Shanzid","Mobile":"01814168127","Address":"Basundhara R/A"}#dictionary
+i={"Apple","Apple","Banana","Mango",10,20,10}#set
+j=frozenset({"Apple","Apple","Banana"})#frozen Set
+h=True#Boolean
+i=b"Hello"#byte
+j=bytearray(b"HI")#bytearray
+k=memoryview(b"Hello")#memoryview
+l=None
+
+
+
+
+

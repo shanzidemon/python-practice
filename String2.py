2b283b57254d6f38367ed31703f5dcf59388619d
shanzidemon <shanzidemon@gmail.com>
2025-08-25 09:15:26 +0600

    add some more practice

---
+++ String2.py
@@ -0,0 +1,12 @@
+x="Shanzid"
+print(x[2:5])
+print(x[:5])
+print(x[2:])
+print(x.upper())
+print(x.lower())
+print(x.capitalize())
+#white space remove kora
+y="     Hello I am shanzid.I am a genious Student     "
+print(y.strip())
+print(y.lstrip())#left strip remove kora.
+print(y.rstrip())#right strip remove kora.
